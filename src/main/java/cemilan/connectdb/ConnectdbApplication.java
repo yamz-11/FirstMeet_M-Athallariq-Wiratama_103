9d4e2d5c98dc6c85c898b0de7c4c359878fd66a7
yamz-11 <wiratamajrz@gmail.com>
2022-10-17 18:12:43 +0700

    tugas cemilan koneksi db

---
+++ src/main/java/cemilan/connectdb/ConnectdbApplication.java
@@ -0,0 +1,13 @@
+package cemilan.connectdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectdbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cemilan/connectdb/ConnectdbApplicationTests.java
@@ -0,0 +1,13 @@
+package cemilan.connectdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
